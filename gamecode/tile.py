f5577c1b2b604b94018c6a5238195f67d6994745
davidddx <david0319072@gmail.com>
2023-12-18 00:03:13 -0500

    nothing

--- gamecode/tile.py
+++ gamecode/tile.py
@@ -4,5 +4,5 @@ from gamecode.settings import TILE_SIZE
 from ECS.VisibleObject import VisibleObject;
 
 class Tile():
-    def __init__(self, x, y, imagedir, isCollidable=false):
+    def __init__(self, x, y, imagedir, isCollidable=False):
         self.visibleobject = VisibleObject(spritedir=imagedir, x=x * TILE_SIZE, y=y * TILE_SIZE);
