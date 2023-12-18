974179928f71c55d708cd093d85b2c31b129fa8d
davidddx <david0319072@gmail.com>
2023-12-18 00:01:42 -0500

    added name property to physics component, added position component

--- ECS/Physics.py
+++ ECS/Physics.py
@@ -1,6 +1,13 @@
 from ECS.Entity import Component
+
+class Position(Component):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
 class Physics(Component):
-    def __init__(self, xvelocity=0, yvelocity=0, gravitationalforce=1):
+    def __init__(self, xvelocity=0, yvelocity=0, gravity=1):
+        self.name = "Physics"
         self.xvelocity = xvelocity;
         self.yvelocity = yvelocity;
-        self.gravitationalforce = gravitationalforce
+        self.gravity = gravity
