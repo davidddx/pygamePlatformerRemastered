8cdd31e0304bae8fef7779049d747ed26e15c39e
davidddx <david0319072@gmail.com>
2023-12-18 00:02:24 -0500

    eliminated x & y component from visibleobject class

--- ECS/VisibleObject.py
+++ ECS/VisibleObject.py
@@ -3,18 +3,11 @@ from ECS.Entity import Component
 import gamecode.settings
 from debug.logger import logger
 #image class takes the following arguments
-class VisibleObject(Component ,pygame.sprite.Sprite):
-    def __init__(self, spritedir, x=0, y=0):
+class VisibleObject(Component):
+    def __init__(self, spritedir):
+        self.name = "VisibleObject"
         pygame.sprite.Sprite.__init__(self);
         self.sprite = pygame.image.load(spritedir)
-        self.rect = self.sprite.get_rect();
-        self.rect.center = (x,y);
-        self.width = self.rect.width;
-        self.height = self.rect.height;
-
-    def update(self):
-        screen = pygame.display.get_surface()
-        if not screen:
-            return None
-
-        screen.blit(self.sprite, (self.rect.x, self.rect.y))
+        rect = self.sprite.get_rect();
+        self.width = rect.width;
+        self.height = rect.height;
