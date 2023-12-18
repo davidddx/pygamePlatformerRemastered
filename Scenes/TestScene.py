ce05b0e84d20569b1e73842b2f808d693cb8e1d1
davidddx <david0319072@gmail.com>
2023-12-18 11:03:29 -0500

    added start and update methods

---
+++ Scenes/TestScene.py
@@ -0,0 +1,8 @@
+from ECS.Scene import Scene
+from gamecode.settings import true, false
+from debug.logger import logger
+
+def start():
+    logger.info(f"starting TestScene.py")
+def update():
+    logger.info(f"updating TestScene.py")
