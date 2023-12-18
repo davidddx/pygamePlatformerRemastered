d0ea5b5bcfd756e176885b7019ab7cfd2558d639
davidddx <david0319072@gmail.com>
2023-12-18 00:01:10 -0500

    added getComponent function, removed parent variable

--- ECS/Entity.py
+++ ECS/Entity.py
@@ -1,22 +1,30 @@
 from abc import ABC, abstractmethod
 
 class Component:
+    def __init__(self):
+        self.name = ""
+
     @abstractmethod
     def update(self):
         pass
 
 
 class Entity:
-    def __init__(self, parent=None, name="", tags=[], components = []):
+    def __init__(self, name="", tags=[], components = []):
         self.name = name
         self.tags = tags
-        self.parent = parent
         self.components = components
 
     def addTag(self, tag):
         self.tags.append(tag);
     def addComponent(self, component):
         self.components.append(component);
+
+    def getComponent(self, componentname):
+        for component in self.components:
+            if component.name == componentname:
+                return component;
+
 
     def update(self):
         for component in self.components:
