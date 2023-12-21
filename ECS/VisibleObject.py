2324fd51981b4a4bf1c67319456683be55ab0a95
davidddx <david0319072@gmail.com>
2023-12-20 23:22:33 -0500

    eliminated pygame.sprite class initialization in VisibleObject.py

--- ECS/VisibleObject.py
+++ ECS/VisibleObject.py
@@ -6,7 +6,6 @@ from debug.logger import logger
 class VisibleObject(Component):
     def __init__(self, spritedir):
         self.name = "VisibleObject"
-        pygame.sprite.Sprite.__init__(self);
         self.sprite = pygame.image.load(spritedir)
         rect = self.sprite.get_rect();
         self.width = rect.width;
