343cfc83125e817e7f3e26be9ad288ca7b8a38d1
davidddx <david0319072@gmail.com>
2023-12-21 16:27:08 -0500

    Removed VisibleObject from Tile class

--- ECS/Tile.py
+++ ECS/Tile.py
@@ -1,33 +1,51 @@
-import pygame.sprite
-
+import pygame
 from gamecode.settings import TILE_SIZE, COMPONENT_TILEMAP
 from ECS.VisibleObject import VisibleObject;
 from ECS.Physics import Position
 from ECS.Entity import Component
+from debug.logger import logger
 
-class Tile(Component):
-    def __init__(self, x : int, y : int, imagedir : str, isCollidable=False):
-        self.visibleobject = VisibleObject(spritedir=imagedir);
+class Tile(Component, pygame.sprite.Sprite):
+    def __init__(self, x : int, y : int, image : pygame.sprite.Sprite, isCollidable=False):
+        pygame.sprite.Sprite.__init__(self);
+        self.image = image;
         self.Position = Position(x=x*TILE_SIZE, y=y*TILE_SIZE)
 
 
 class Tilemap(Component):
-    def __init__(self, tiles : list[list[str]]):
+    def __init__(self, tiles : list[list[str]], Tileset = {}):
         self.name = COMPONENT_TILEMAP
+        self.Tileset = self.loadImages(Tileset=Tileset) #Dictionary must follow format {'Char' : 'ImageDirectory'}
         self.Tiles = self.loadTiles(tileList= tiles);
 
+    def loadImages(self, Tileset : dict):
+        newdict = dict()
+        keys = list(Tileset.keys())
+        sprite = pygame.sprite.Sprite()
+        for key in keys:
+            sprite.image = pygame.image.load(Tileset[key])
+            newdict[key] = sprite.image
+        return newdict
     def loadTiles(self, tileList):
         spritegroup = pygame.sprite.Group()
         for yval, alist in enumerate(tileList):
             for xval, letter in enumerate(alist):
-                if letter == 'S':
-                    logger.debug(f"{xval=} | {yval=}")
-                    tile = Tile(x=xval, y=yval, imagedir="TileImages/BasicBlueSkyTile.png")
-                    spritegroup.add(tile.visibleobject)
-                    logger.debug(f"{tile=} | {xval=} | {yval=}")
-                elif letter == 'B':
-                    pass
-                else:
-                    pass
+                if not letter in self.Tileset:
+                    logger.error(f"Tile at ({xval=},{yval=}) of tilemap {self} is not defined in self.Tileset.")
+                    continue;
+                tile = Tile(x=xval, y=yval, image=self.Tileset[letter])
+                logger.debug(f"{tile=} | {xval=} | {yval=}")
+                logger.debug(f"{tile.image=}")
+                spritegroup.add(tile)
+
+                # if letter == 'S':
+                #     logger.debug(f"{xval=} | {yval=}")
+                #     tile = Tile(x=xval, y=yval, imagedir="TileImages/BasicBlueSkyTile.png")
+                #     spritegroup.add(tile.visibleobject)
+                #     logger.debug(f"{tile=} | {xval=} | {yval=}")
+                # elif letter == 'B':
+                #     pass
+                # else:
+                #     pass
         return spritegroup
 
