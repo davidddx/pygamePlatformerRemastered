a094675668b1ca3ee4ed98ef12a0ab5300a94226
davidddx <david0319072@gmail.com>
2023-12-28 16:20:08 -0500

    added a name

--- ECS/Collision.py
+++ ECS/Collision.py
@@ -1,8 +1,10 @@
 import pygame
 from ECS.Entity import Component
+from gameocde.settings import COMPONENT_COLLISION
 #Entities with Collision component collide with entities with collision Component
 class Collision(Component):
     def __init__(self, spriteSurf : pygame.Surface, type="rect"):
+        self.name = COMPONENT_COLLISION
         collsurf = None
         if type == "rect":
             collsurf = spriteSurf.get_rect()
