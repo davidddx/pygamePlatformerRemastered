a217f1512eca9e740ef341db60da49d3e4673a57
davidddx <david0319072@gmail.com>
2023-12-28 16:21:02 -0500

    added collision handler

--- ECS/Systems.py
+++ ECS/Systems.py
@@ -7,8 +7,9 @@ class Systems:
     def PhysicsProcess(entity : Entity):
         physicsComponent = entity.getComponent(COMPONENT_PHYSICS)
         if not physicsComponent: return None;
-        
-        pass #physicsStuff
+        posComponent = entity.getComponent(COMPONENT_POSITION)
+        if not posComponent: logger.error(f"{entity.name = } Has {COMPONENT_PHYSICS} component but no {COMPONENT_POSITION} component")
+        posComponent.x += physicsComponent.xvelocity
     @staticmethod
     def DisplayProcess(entity : Entity, screen : pygame.surface):
         visComponent = entity.getComponent(COMPONENT_VISIBLE_OBJECT)
@@ -27,6 +28,23 @@ class Systems:
         for tile in tilemapComponent.Tiles:
             screen.blit(tile.image, (tile.Position.x, tile.Position.y))
 
+    def HandleCollision(self, entities : list):
+        for i in range(len(entities)):
+            collision_component1 = entity[i].getComponent(COMPONENT_COLLISION)
+            if not collision_component1:
+                continue
+            logger.debug(f"{entities[i]=} has Collision Component")
+            for j in range(i + 1, len(entities)):
+                collision_component2 = entity[j].get_component(COMPONENT_COLLISION)
+                if not collision_component2:
+                    continue;
+                logger.debug(f"{entities[j]=} has Collision Component")
+                if not (collision_component1.rect.colliderect(collision_component2.rect)
+                ):
+                    continue;
+                logger.debug(f"{entities[i]=} and {entities[j]=} have collided")
+
+
     @staticmethod
     def Update(entities : list):
         screen = pygame.display.get_surface()
@@ -34,3 +52,4 @@ class Systems:
             Systems.DisplayProcessTilemap(entity= entity, screen=screen)
             Systems.DisplayProcess(entity= entity, screen=screen)
             Systems.PhysicsProcess(entity= entity)
+        Systems.HandleCollision(entities)
