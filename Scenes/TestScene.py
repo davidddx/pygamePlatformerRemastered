631574674b1bf0f969ec93032adc217ae3a59c7c
davidddx <david0319072@gmail.com>
2023-12-30 22:02:46 -0500

    testing

--- Scenes/TestScene.py
+++ Scenes/TestScene.py
@@ -1,7 +1,7 @@
 from ECS.Scene import Scene
 from ECS.Entity import Entity
 from ECS.Physics import Physics
-from ECS.VisibleObject import VisibleObject
+from ECS.VisibleObject import VisibleObject, InvisibleObject
 from ECS.Tile import Tilemap
 from gamecode.settings import true, false, TILE_SIZE, SCREEN_WIDTH, SCREEN_HEIGHT
 from debug.logger import logger
@@ -10,13 +10,18 @@ from gamedata.Maps import World1
 cwd = os.getcwd()
 sceneobj = Scene()
 blueskytiledir = cwd + '/TileImages/BasicBlueSkyTile.png'
-tmap = Entity(name="tmap")
-tmapComponent = Tilemap(tiles= World1.Level1, Tileset= {'S' : blueskytiledir} )
-tmap.addComponent(tmapComponent)
+
+phystest = Entity(name="phystest")
+phystest.addComponent(VisibleObject(spritedir=blueskytiledir))
+phystest.addComponent(Physics(xvelocity = 0.8, yvelocity=0.001, gravity = 0.01))
+sceneobj.addentity(phystest)
+
+# tmap = Entity(name="tmap")
+# tmapComponent = Tilemap(tiles= World1.Level1, Tileset= {'S' : blueskytiledir} )
+# tmap.addComponent(tmapComponent)
 # physComponent = Physics(xvelocity= 0.001, yvelocity= 0.001, gravity=1)
-# tmap.addComponent(physComponent)
-# tmap.addComponent(posComponent)
-sceneobj.addentity(tmap)
+# tmap.addCbygomponent(physComponent)
+# sceneobj.addentity(tmap)
 
 # image = Entity(name="image")
 # image.addComponent(Position(x=0, y=0))
